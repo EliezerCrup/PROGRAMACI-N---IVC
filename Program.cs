eaa45232a6e5f6de21d6194b7e44ae23404ad7c2
EliezerCrup <ecrup@donbosco.edu.gt>
2026-03-17 10:52:44 -0600

    Add files via upload
    Trabaje en clases

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//Un programa que solicite al usuario un valor numerico
+//Mostrar en pantalla si positivo o negativo
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Numeros Positivos y Negativos");
+        //Varibles
+        int num;
+        num = Convert.ToInt32(Console.ReadLine());
+
+        if (num >= 0)
+            Console.WriteLine("Soy un numero POSITIVO");
+        else
+            Console.WriteLine("Soy un numero NEGATIVO");
+
+    }
+}
